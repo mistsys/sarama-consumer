9d729ea5bc59507b51c76f4719d97b405fee4add
Yuan-Hsiang Lee <yhlee@mistsys.com>
2024-08-23 09:52:38 -0600

    adjust bucket logging

--- consumer.go
+++ consumer.go
@@ -1709,7 +1709,7 @@ func (con *consumer) run(wg *sync.WaitGroup) {
 			msg.Topic = con.topic
 		}
 
-		dbgf("consumer done(%q:%d/%d)", msg.Topic, msg.Partition, msg.Offset)
+		msgf("consumer done(%q:%d/%d)", msg)
 
 		// a sanity check, just in case someone passes the msg into the wrong consumer
 		if con.topic != msg.Topic {
@@ -1741,7 +1741,10 @@ func (con *consumer) run(wg *sync.WaitGroup) {
 				return
 			}
 			part.buckets[index].done++
-			dbgf("bucket[%d/%d] read %d done %d", index, len(part.buckets), part.buckets[index].read, part.buckets[index].done)
+			dbgf("bucket[%d/%d] read %d done %d (on %s/%d/%d)", index, len(part.buckets),
+				part.buckets[index].read, part.buckets[index].done,
+				msg.Topic, msg.Partition, msg.Offset,
+			)
 			if index == 0 {
 				// we might be able to advance the bucket 0 highwater mark
 				if part.buckets[0].read == part.buckets[0].done {
